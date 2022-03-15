da562a1863e2df3d41997edbcda8f465d384720f
Ramiro Magno <ramiro.magno@gmail.com>
2022-03-15 12:38:20 +0000

    Add new function `list_archives()`

---
+++ R/list_archives.R
@@ -0,0 +1,24 @@
+#' List monthly and quarterly archives
+#'
+#' This function lists the monthly and quarterly archives currently available.
+#'
+#' @return A [tibble][tibble::tibble-package] of available archives for download.
+#' @md
+#' @examples
+#' list_archives()
+#'
+#' @export
+list_archives <- function() {
+
+  monthly_url <- rvest::url_absolute('monthly/tsv', ftp_archive())
+  quarterly_url <- rvest::url_absolute('quarterly/tsv', ftp_archive())
+
+  monthly_tbl <- ftp_ls(monthly_url) %>%
+    dplyr::mutate(series = 'monthly', .before = 1L)
+
+  quarterly_tbl <- ftp_ls(quarterly_url) %>%
+    dplyr::mutate(series = 'quarterly', .before = 1L)
+
+  dplyr::bind_rows(monthly_tbl, quarterly_tbl)
+
+}

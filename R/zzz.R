9e4ab04997f57c7b1190c371ca965ca09a845731
jemunro <jacob.e.munro@gmail.com>
2023-08-30 13:41:26 +1000

    add caching for latest_archive_url

--- R/zzz.R
+++ R/zzz.R
@@ -9,9 +9,17 @@
   )
 
   # cache list_archives in memory for 15 min
+  # speeds up latest_monthly_url and latest_quarterly_url
   list_archives <<- memoise::memoise(
     list_archives,
-    cache = cachem::cache_mem(max_size = 512 * 1024^2, max_age = 60*15)
+    cache = cachem::cache_mem(max_size = 100 * 1024^2, max_age = 60*15)
+  )
+
+  # cache latest_archive_url in memory for 15 min
+  # speeds up import_hgnc_dataset with default file argument
+  latest_archive_url <<- memoise::memoise(
+    latest_archive_url,
+    cache = cachem::cache_mem(max_size = 100 * 1024^2, max_age = 60*15)
   )
 
   invisible()
